4dddf71aa33e571b5c75708bd2e3e54d5a23f14f
IbeSotalin <ibeth.sotalin@epn.edu.ec>
2024-07-03 10:26:52 -0500

    cambio tu espacio

--- src/Componentes/EditarInformacion.jsx
+++ src/Componentes/EditarInformacion.jsx
@@ -1,34 +1,79 @@
-import React from 'react';
+import React, { useState } from "react";
 import AyudaAnimal from '../Imagenes/img-mascota-5.webp';
 import '../Estilos/StyleUI4.css'
 import '../Estilos/StyleInformacion.css'
 import '../Estilos/StyleRegistrar.css'
 import { useNavigate } from 'react-router-dom';
+///
+import { ReactComponent as IconoSVG } from '../icons/icono-usuario.svg';
+import imagen from '../Imagenes/icono2.jpg';
+import '../Estilos/StyleUI4.css'
+import '../Estilos/StyleInformacion.css'
+import '../Estilos/StyleRegistrar.css'
+import logo from '../Imagenes/logo.webp'
+import imgPr from '../Imagenes/espacio.png'
 
-function EditarInformacion (){
+function EditarInformacion() {
 
   const navigate = useNavigate();
 
-  const handleTerminar = () =>{
+  const handleTerminar = () => {
     navigate('/espacio')
   }
-    
-    return (
-        <div className='containerT'>
-            <h2>Datos de la mascota</h2>
-            <section className="form-section">
+  // navbar
+  const [active, setActive] = useState('Registar Mascota');
+  const handleClick = (item) => {
+    if (item === 'Home') {
+      navigate('/login/menu'); // Reemplaza '/home' con la ruta a la que quieres navegar
+    } else {
+      setActive(item);
+    }
+  };
+
+  return (
+    <div className="Container">
+      <header>
+        <div className="user-section">
+          <p><IconoSVG className="icon" />Usuario</p>
+        </div>
+        <h1>ESPERANZA ANIMAL
+          <img src={logo} className='esperanzaImg' alt="Imagen de Esperanza Animal" />
+        </h1>
+      </header>
+      <hr />
+      {/* g */}
+      <div>
+        <nav>
+          {['Tus Mascotas', 'Registrar Mascota', 'Editar Información', 'Home'].map((item) => (
+            <button
+              key={item}
+              className={`nav-button ${active === item ? 'active' : ''}`}
+              onClick={() => handleClick(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </nav>
+        <img src={imgPr} className='presentacionImg' alt="Imagen de Presentación" ></img>
+
+      </div>
+
+      {/* g */}
+      <div className='containerT'>
+        <h2>Datos de la mascota</h2>
+        <section className="form-section">
           <form>
             <div className="form-container">
               {/* Contenedor 1 */}
               <div className="form-group-container">
                 <div className="form-group">
-                    <div className='form-group1'>
-                      <div>
+                  <div className='form-group1'>
+                    <div>
                       <label>Ingrese el Nombre de su Mascota</label>
                       <input type="text" placeholder="Lucho" className='Mascota' />
-                      </div>                     
                     </div>
-                </div>         
+                  </div>
+                </div>
                 <div className="form-group">
                   <label>Seleccione su tipo de Mascota</label>
                   <select className="small-select">
@@ -63,11 +108,11 @@ function EditarInformacion (){
               <div className="form-group-container">
                 <div className="form-group">
                   <label>Describa la Personalidad de su Mascota</label>
-                  <input type="text" placeholder="Es una mascota..." className="estado"/>
+                  <input type="text" placeholder="Es una mascota..." className="estado" />
                 </div>
                 <div className="form-group">
                   <label>Describa Rasgos Distintivos de su Mascota</label>
-                  <input type="text" placeholder="Tiene un ojo de otro color..." className="estado"/>
+                  <input type="text" placeholder="Tiene un ojo de otro color..." className="estado" />
                 </div>
               </div>
               {/* Contenedor 4 */}
@@ -81,10 +126,12 @@ function EditarInformacion (){
             </div>
           </form>
         </section>
-        </div>
-        
-      );
-    };
+      </div>
+    </div>
+
+
+  );
+};
 
 export default EditarInformacion;
 
