a59c11961efc5d9af689028fe3cc21ce322c9dde
XAVIER ALEJANDRO SIMBANA ZUNA <xavier.simbana@epn.edu.ec>
2024-07-08 09:03:33 -0600

    modifique el db.json y ya puedo agregar mascotas

--- src/Componentes/RegistrarMascota.jsx
+++ src/Componentes/RegistrarMascota.jsx
@@ -20,9 +20,7 @@ function RegistrarMascota() {
   const [nombre, setNombre] = useState('');
   const navigate = useNavigate();
 
-  const handleAñadir = () => {
-    navigate('/espacio');
-  }
+  
   useEffect(() => {
     // Fetch the name associated with the id from the server
     const fetchName = async () => {
@@ -62,8 +60,43 @@ function RegistrarMascota() {
         fetchImage();
     }, [id]);
 
+    //Capturar todos los atributos de la mascota que se quiere agregar
+    const [datosMascota, setDatosMascota] = useState({
+      nombre: '',
+      tipo: '',
+      raza: '',
+      sexo: '',
+      color: '',
+      tamaño: '',
+      personalidad: '',
+      rasgosDistintivos: '',
+      fotoMascota:''
+    });
+
+    const handleChange = (e) => {
+      setDatosMascota({
+        ...datosMascota,
+        [e.target.id]: e.target.value
+      });
+    };
+
+// enviar la informacion al db.json
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    await axios.post("http://localhost:5000/mascota", datosMascota);
+    console.log("Data sent successfully!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+const handleAñadir = () => {
+  handleChange();
+  navigate(`/espacio/${id}`);
+}
+
   return (
-    <div className="Container">
+    <div className="Container" onSubmit={handleSubmit}>
       <header>
         <div className="user-section">
         <p style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
@@ -77,8 +110,8 @@ function RegistrarMascota() {
       </header>
       <hr />
       {/* g */}
-      <div>
-        <nav>
+        <div>
+          <nav>
           {['Tus Mascotas', 'Registrar Mascota', 'Editar Información', 'Home'].map((item) => (
             <button
               key={item}
@@ -107,19 +140,20 @@ function RegistrarMascota() {
                   <div className='form-group1'>
                     <div>
                       <label>Ingrese el Nombre de su Mascota</label>
-                      <input type="text" placeholder="Fred" className='Mascota' />
+                      <input type="text" placeholder="Fred" className='Mascota' id="nombre" value={datosMascota.nombre} onChange={handleChange}/>
                     </div>
                   </div>
                 </div>
                 <div className="form-group">
                   <label>Seleccione su tipo de Mascota</label>
-                  <select className="small-select">
+                  <select className="small-select" id="tipo" value={datosMascota.tipo} onChange={handleChange}>
                     <option>Gato</option>
+                    <option>Perro</option>
                   </select>
                 </div>
                 <div className="form-group">
                   <label>Seleccione la Raza de su Mascota</label>
-                  <select className="small-select">
+                  <select className="small-select" id="raza" value={datosMascota.raza} onChange={handleChange}>
                     <option>Indeterminada</option>
                   </select>
                 </div>
@@ -128,38 +162,45 @@ function RegistrarMascota() {
               <div className="form-group-container">
                 <div className="form-group">
                   <label>Seleccione el Sexo de su Mascota</label>
-                  <select className='Mascota'>
+                  <select className='Mascota' id="sexo" value={datosMascota.sexo} onChange={handleChange}>
                     <option>Macho</option>
+                    <option>Hembra</option>
                   </select>
                 </div>
                 <div className="form-group">
                   <label>Ingrese el Color de su Mascota</label>
-                  <input type="text" placeholder="Negro" className='Mascota' />
+                  <input type="text" placeholder="Negro" className='Mascota' id="color" value={datosMascota.color} onChange={handleChange}/>
                 </div>
                 <div className="form-group">
                   <label>Ingrese el Tamaño de su mascota</label>
-                  <input type="text" placeholder="1.25 m" className='Mascota' />
+                  <input type="text" placeholder="1.25 m" className='Mascota' id="tamaño" value={datosMascota.tamaño} onChange={handleChange}/>
                 </div>
               </div>
               {/* Contenedor 3 */}
               <div className="form-group-container">
                 <div className="form-group">
                   <label>Describa la Personalidad de su Mascota</label>
-                  <input type="text" placeholder="Es una mascota..." className="estado" />
+                  <input type="text" placeholder="Es una mascota..." className="estado" id="personalidad" value={datosMascota.personalidad} onChange={handleChange}/>
                 </div>
                 <div className="form-group">
                   <label>Describa Rasgos Distintivos de su Mascota</label>
-                  <input type="text" placeholder="Manchas blancas..." className="estado" />
+                  <input type="text" placeholder="Manchas blancas..." className="estado" id="rasgosDistintivos" value={datosMascota.rasgosDistintivos} onChange={handleChange}/>
                 </div>
               </div>
               {/* Contenedor 4 */}
               <div className="form-group-container">
                 <div className="form-group photo-section">
-                  <label>Agregar Fotografía</label>
+                  <label id="fotoMascota" value={datosMascota.fotoMascota} onChange={handleChange} type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{
+                                mt: 3, mb: 2, backgroundColor: "#754a36",
+                                color: "white"
+                            }}
+                            onClick={handleChange}>Agregar Fotografía</label>
                   <button className="add-photo-button">+</button>
                 </div>
-                <button onClick={handleAñadir} type="submit" className="submit-button">Añadir <span className="check-icon">✔</span></button>
-              </div>
+                <button onClick={handleAñadir} type="submit" className="submit-button">Añadir <span className="check-icon">✔</span></button>              </div>
             </div>
           </form>
         </section>
