2df7debfde3e35aff047da6c7a453e9115ba19ba
diego99m <diegomoya1399@gmail.com>
2024-08-15 10:21:34 -0500

    Modifique la ruta (NO TOPAR)

--- servidor/routes/user.routes.js
+++ servidor/routes/user.routes.js
@@ -7,11 +7,11 @@ module.exports = function (app, fileupload, fileupload2,fileupload3) {
     app.post('/usuarios/new', fileupload, UserController.createUser);
     app.get('/usuarios', UserController.getAllUsers);
     app.get('/usuarios/:id', UserController.getUser);
+    //Para mascotas
     app.post('/mascotas/new', fileupload3, MascotaController.createMascota);
     app.get('/mascotas', MascotaController.getAllMascotas);
     app.get('/mascotas/:id', MascotaController.getMascota);
     app.put('/mascota/editar/:id', MascotaController.updateMascota)
-
     //para animales abandono
     app.post('/animal', fileupload2, animalController.createAnimal);
     app.get('/animal', animalController.getAllAnimals);
